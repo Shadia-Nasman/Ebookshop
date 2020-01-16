44c72b53590e399165c61d4658abe0aebda4593c
Shadia <53862042+Shadia-Nasman@users.noreply.github.com>
2020-01-16 09:17:51 +0100

    Update control.php

--- control/control.php
+++ control/control.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
   function datastore($email,$firstname,$lastname,$username,$password,$gsm ,$cardno,$nameoncard,$address){
     $db = openConnection();
           $select = $db->query("INSERT INTO
@@ -29,8 +27,54 @@
         '$gsm',
         '$cardno',
         '$nameoncard',
-        '$address'      
+        '$address',      
       )
-  "  );
+  " );
     }
 
+        // for login //
+  function datastore1($email,$password)
+    {
+      $db = openConnection();
+      $select = $db->prepare("SELECT * from user where email = '$email' and password = '$password'");
+      $select->execute();
+      $row_count = $select->rowCount();
+      var_dump($row_count);
+
+        if ($row_count > 0) 
+          {
+            header('location: products.php');
+          } 
+        else 
+        {
+            echo "password not found please check if you are registered";
+        }
+    
+      }
+    
+      // for checkout //
+
+      function check($orderid,$bookid,$email,$price,$totalprice,$shippingaddress,$bookcount){
+        $db = openConnection();
+              $select = $db->query("INSERT INTO
+            `order`  (
+                orderid,
+                bookid,
+                email,
+                price,
+                totalprice,
+                shippingaddress,
+                bookcount
+              )
+      VALUES 
+        (
+            '$orderid',
+            '$bookid',
+            '$email' 
+            '$price',
+            '$totalprice',
+            '$shippingaddress',
+            '$bookcount'
+          )
+      "  );
+        }
