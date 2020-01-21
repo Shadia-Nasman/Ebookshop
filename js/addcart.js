cd335acc43aa82add2fd19f2d21a32e9e9ffd983
shadia <shadia.nasman1988@gmail.com>
2020-01-21 16:07:13 +0100

    improve

--- js/addcart.js
+++ js/addcart.js
@@ -1,19 +1,20 @@
 buttons.addEventListener('click', event => {
-        event.preventDefault();
-        console.log(event.target.id);
-        var idval=event.target.id;
-        getbook (idval)
-        async function getbook (id) {
-          console.log(id);
-         var respons = await fetch(`getbook.php?q=${id}`);
-         var jsonresponse = await respons.json();
-         console.log(jsonresponse);
-       
-      
-        
-        sessionStorage.setItem("jsonresponse", JSON.stringify(jsonresponse));
+    event.preventDefault();
+    console.log(event.target.id);
+    var idval = event.target.id;
+    getbook(idval)
+    async function getbook(id) {
+        console.log(id);
+        var respons = await fetch(`getbook.php?q=${id}`);
+        var jsonresponse = await respons.json();
+        console.log(jsonresponse);
 
-         } 
-       
+
+
+        sessionStorage.setItem("jsonresponse", JSON.stringify(jsonresponse));
         window.location.href = "item.php";
-      });
+
+    }
+
+
+});
